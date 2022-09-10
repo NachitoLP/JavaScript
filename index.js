6a2649d9c98d00eede1c0e9ac30c14a238961c80
Joaquin <joaquinacholopez@gmail.com>
2022-09-10 14:57:07 -0300

    primera entrega

--- index.js
+++ index.js
@@ -35,6 +35,30 @@ function vender() {
     }
 }
 
+function calculadora(){
+    let numero1 = parseInt(prompt("Ingrese un número:"));
+    let numero2 = parseInt(prompt("Ingrese otro número:"));
+    let op = prompt("Ingrese la operación a realizar:").toLowerCase();
+    if (op === "suma"){
+        let resultado = numero1 + numero2;
+        alert(`El resultado de la suma de ${numero1} y ${numero2} es ${resultado}`);
+    }else if (op === "resta"){
+        let resultado = numero1 - numero2;
+        alert(`El resultado de la resta de ${numero1} y ${numero2} es ${resultado}`);
+    }else if (op === "multiplicacion"){
+        let resultado = numero1 * numero2;
+        alert(`El resultado de la multiplicación de ${numero1} y ${numero2} es ${resultado}`);
+    }else if (op === "division"){
+        if (numero2 === 0){
+            alert("No se puede dividir por 0, ingrese los números nuevamente.");
+            calculadora();
+        }else{
+            let resultado = numero1 / numero2;
+            alert(`El resultado de la división de ${numero1} y ${numero2} es ${resultado}`);
+        }
+    }
+}
+
 function ingreso(){
     for(let x = 1 ; x <=100 ; x++){
         let nombre = prompt("Ingrese su nombre");
@@ -48,16 +72,16 @@ function ingreso(){
         }else if (edad >= 18){
             let mensaje2 = `${nombre}, sos mayor de edad, podés seguir.`;
             alert(mensaje2);
-            let confirmacion = prompt("¿Te interesa saber que productos ofrecemos?").toLowerCase();
+            let confirmacion = prompt("Ingrese 1 si quiere acceder a la calculadora, o 2 para el catálogo de objetos.").toLowerCase();
             if (confirmacion === ""){
                 alert("Lo sentimos, el valor ingresado no es correcto, vuelva a ingresar otro valor")
                 ingreso();
             }
-            else if (confirmacion === "no"){
-                alert("Ok, ten un buen día.")
+            else if (confirmacion === "1"){
+                calculadora();
             }
-            else if (confirmacion === "si"){
-                alert("Ok, los productos son los siguientes:");
+            else if (confirmacion === "2"){
+                alert("Los productos son los siguientes:");
                 producto();
                 vender();
             }
