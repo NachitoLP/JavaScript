4c030f529369e0ae22d3a3a4151969401cf57bf3
Joaquin <joaquinacholopez@gmail.com>
2022-09-12 16:12:12 -0300

    Título mejorado

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function ingreso() {
     alert("Bienvenido a mi sitio web!");
     let nombre = prompt("Ingrese su nombre.");
-
+    if (nombre === "") {
+        alert("Ese no es un nombre válido. Vuelva a ingresarlo.");
+        ingreso();
+        h2.innerHTML = "";
+    }
     let contenedor = document.getElementById("div_titulo");
     let h2 = document.createElement("h2");
     h2.innerHTML = `<center>Bienvenido, ${nombre}. Probá cambiar el color de la barra de navegación. ¡Es fácil!</center>`;
