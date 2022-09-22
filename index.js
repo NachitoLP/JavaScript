317cc4995bf2ae6f6796a741b5ea924cac88d6b7
Joaquin <joaquinacholopez@gmail.com>
2022-09-22 18:28:47 -0300

    carrito sin terminar

--- index.js
+++ index.js
@@ -20,6 +20,62 @@ function ingreso() {
     })
 }
 
+function carrito() {
+    let contenedor = document.getElementById("carrito");
+    let carrito = [];
+    let carritoStorage = JSON.parse(localStorage.getItem("carrito"));
+    
+    const productos = [
+        {id: 1, nombre: "camisa", precio: "$5500"},
+        {id: 2, nombre: "medias", precio: "$1000"},
+        {id: 3, nombre: "zapatillas", precio: "$8000"},
+        {id: 4, nombre: "gorra", precio: "$3000"},
+        {id: 5, nombre: "guantes", precio: "$1200"},
+        {id: 6, nombre: "pantalón", precio: "$9000"}
+    ]
+    localStorage.setItem("carrito", JSON.stringify(productos));
+
+    if(carritoStorage){
+        carrito = carritoStorage;
+    }
+
+    carrito.forEach(producto => {
+        let div = document.createElement("div");
+        div.innerHTML = `
+            <h3>ID: ${producto.id}</h3>
+            <p>Nombre: ${producto.nombre}</p>
+            <p>Precio: ${producto.precio}</p>
+        `;
+        contenedor.append(div);
+    });
+
+    function busqueda() {
+        let contenedorBusqueda = document.getElementById("formulario_busqueda");
+        let contenedorCarrito = document.getElementById("contenedor_carrito");
+        let busqueda = document.getElementById("input_busqueda");
+        let condicion = false;
+        contenedorBusqueda.addEventListener("submit", (e) => {
+            e.preventDefault();
+            condicion = true;
+            if(condicion === true){
+                contenedor.className = "section_desaparece";
+            }
+
+            productoBuscado = busqueda.value.toLowerCase();
+            let producto = productos.find(producto => producto.nombre === productoBuscado);
+            let div2 = document.createElement("div");
+            div2.innerHTML = `
+                <h3>ID: ${producto.id}</h3>
+                <p>Nombre: ${producto.nombre}</p>
+                <p>Precio: ${producto.precio}</p>
+            `;
+            contenedorCarrito.append(div2);
+            div2.className = "resultado_busqueda";
+        })
+    } 
+    busqueda();
+}
+
 const botonRojo = () => {
     const respuesta = () => {
         nav.className = "rojo";
@@ -51,6 +107,7 @@ const botonVerde = () => {
 }
 
 ingreso();
+carrito();
 botonRojo();
 botonAzul();
 botonVerde();
